4941bb3f65bec6ea7989e095fa94eed8ad13dcf5
FROEDERH <henriquefroeder52@gmail.com>
2021-08-24 20:08:07 -0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,16 @@ export default function Home() {
   const [list, setList] = useState(null);
   const [isLoading, setLoading] = useState(true);
 
+  function SearchInAPI(){
+    setIsLoading(true);
+    fetch(`https://www.omdbapi.com/?apikey=8da00471&s=`)
+    .then(x => x.json())
+    .then(x => {
+      setIsLoading(false);
+      setList(x);
+    });
+  }
+
   return (
     <div>
       <h1>Buscando por {search}</h1>
