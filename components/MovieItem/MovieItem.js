6d5aad89b7df8f8e7379d4cc83cd4c935d1f28b5
FROEDERH <henriquefroeder52@gmail.com>
2021-08-24 21:50:20 -0300

    2 files modified

--- components/MovieItem/MovieItem.js
+++ components/MovieItem/MovieItem.js
@@ -2,7 +2,7 @@ import styles from './MovieItem.module.css';
 
 export default function MovieItem({ item, ...props }) {
   return (
-    <div className={styles.h1} className={styles.content_ballon}>
+    <div className={styles.h1}>
       <img className={styles.img1} src={item.Poster} />
       {item.Title}
     </div>
