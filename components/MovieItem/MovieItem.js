c8ea5465827a5246873ed5e0f91f07d865865f1b
FROEDERH <henriquefroeder52@gmail.com>
2021-08-31 21:17:18 -0300

    1 file modified

--- components/MovieItem/MovieItem.js
+++ components/MovieItem/MovieItem.js
@@ -3,7 +3,7 @@ import styles from './MovieItem.module.css';
 export default function MovieItem({ item, ...props }) {
   return (
     <div className={styles.item}>
-      <img className={styles.itemimg} src={item.Poster} />
+      <img className={styles.item img} src={item.Poster} />
       {item.Title}
     </div>
   );
