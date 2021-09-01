3369042d354b48257bfb001bbb630e762628a8a0
FROEDERH <henriquefroeder52@gmail.com>
2021-08-31 21:31:59 -0300

    1 file modified

--- components/MovieItem/MovieItem.js
+++ components/MovieItem/MovieItem.js
@@ -1,10 +1,23 @@
+import Link from 'next/dist/client/link';
+import { Span } from 'next/dist/telemetry/trace';
+import link from 'next/link';
 import styles from './MovieItem.module.css';
 
 export default function MovieItem({ item, ...props }) {
   return (
-    <div className={styles.item}>
-      <img className={styles.itemimg} src={item.Poster} />
-      {item.Title}
-    </div>
+    <Link href={`movie/${item.imdbID}`}>
+      <a className={styles.item}>
+        <img className={styles.itemimg} src={item.Poster} />
+
+        <span className={styles.Year} src={item.Year}>
+          {' '}
+        </span>
+
+        <span className={styles.Title} src={item.Title}>
+          {' '}
+          {item.Title}{' '}
+        </span>
+      </a>
+    </Link>
   );
 }
