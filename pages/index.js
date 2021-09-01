d41f36ed468c9370c3901008b223dcd1f9c1ccb9
FROEDERH <henriquefroeder52@gmail.com>
2021-08-31 21:13:12 -0300

    2 files modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,32 @@
 import { useState, useEffect } from 'react';
 import MovieList from '../components/MovieList/MovieList';
+import useDebounce from '../hooks/useDebounce';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const [search, setSearch] = useState('Star Wars');
   const [list, setList] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const debouncedSearch = useDebounce(search, 500);
 
-  function SearchInAPI(){
+  function SearchInAPI() {
     setIsLoading(true);
-    fetch(`https://www.omdbapi.com/?apikey=8da00471&s=${search}`)
-    .then(x => x.json())
-    .then(x => {
-      setIsLoading(false);
-      setList(x.Search);
-    });
+    fetch(`https://www.omdbapi.com/?apikey=8da00471&s=${debouncedSearch}`)
+      .then(x => x.json())
+      .then(x => {
+        setIsLoading(false);
+        setList(x.Search);
+      });
   }
 
-  useEffect ( ()  => {
+  useEffect(() => {
     SearchInAPI();
-   },[search]);
+  }, [debouncedSearch]);
 
-   function handleSearchChanges(event){
-     const value = event.target.value;
-     setSearch(value)
-   }
+  function handleSearchChanges(event) {
+    const value = event.target.value;
+    setSearch(value);
+  }
 
   return (
     <div>
@@ -32,7 +34,7 @@ export default function Home() {
       <h1>Buscando por {search}</h1>
       <div>
         {isLoading && <span>Carregando...</span>}
-       <MovieList list={list} />
+        <MovieList list={list} />
       </div>
     </div>
   );
